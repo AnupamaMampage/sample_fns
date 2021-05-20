70b3b26a81a4429f15bba5358b3f6639e78ec1f8
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-05-20 16:11:08 +1000

    Update float.py

--- float.py
+++ float.py
@@ -4,7 +4,8 @@ import math
 
 def main(event, context):
   start=time()
-  n = int(event['data'])
+#   n = int(event['data'])
+  n = int(sys.argv[1])
   for i in range(0, n):
           sin_i = math.sin(i)
           cos_i = math.cos(i)
