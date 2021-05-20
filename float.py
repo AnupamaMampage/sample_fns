f80eff5bb94c390ea4eab410c0404496e4ec9275
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-05-20 14:50:07 +1000

    Created float.py

---
+++ float.py
@@ -0,0 +1,12 @@
+import time
+from time import time
+import math
+def main(event, context):
+start=time()
+n = int(event['data'])
+for i in range(0, n):
+        sin_i = math.sin(i)
+        cos_i = math.cos(i)
+        sqrt_i = math.sqrt(i)
+latency = time()-start
+return str(latency)
