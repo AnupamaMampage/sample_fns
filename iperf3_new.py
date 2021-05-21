ac01dc7cfbc15544f01902977124cd56db5c22d8
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-05-21 11:20:24 +1000

    Created iperf3_new.py

---
+++ iperf3_new.py
@@ -0,0 +1,31 @@
+import iperf3
+
+def main(event, context):
+    client = iperf3.Client()
+    client.duration = 1
+    client.server_hostname = '45.113.235.133'
+    client.port = 5201
+    client.protocol = 'udp'
+
+
+    print('Connecting to {0}:{1}'.format(client.server_hostname, client.port))
+    result = client.run()
+
+    if result.error:
+        print(result.error)
+    else:
+        print('')
+        print('Test completed:')
+        print('  started at         {0}'.format(result.time))
+        print('  bytes transmitted  {0}'.format(result.bytes))
+        print('  jitter (ms)        {0}'.format(result.jitter_ms))
+        print('  avg cpu load       {0}%\n'.format(result.local_cpu_total))
+
+        print('Average transmitted data in all sorts of networky formats:')
+        print('  bits per second      (bps)   {0}'.format(result.bps))
+        print('  Kilobits per second  (kbps)  {0}'.format(result.kbps))
+        print('  Megabits per second  (Mbps)  {0}'.format(result.Mbps))
+        print('  KiloBytes per second (kB/s)  {0}'.format(result.kB_s))
+        print('  MegaBytes per second (MB/s)  {0}'.format(result.MB_s))
+        
+    retun "Hello"
