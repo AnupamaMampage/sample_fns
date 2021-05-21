0ec8b9e0ecc34761967df46237a92bc09d43c6cf
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-05-21 19:22:47 +1000

    Update speed.py

--- speed.py
+++ speed.py
@@ -1,7 +1,7 @@
 import speedtest
 
 def main(event, context):
-  print(dir(speedtest))
-  return "Hello"
+#   print(dir(speedtest))
+  return (dir(speedtest))
 #   s = speedtest.Speedtest()
 #   return('My download speed is:', s.download())
