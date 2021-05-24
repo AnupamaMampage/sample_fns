9476fa7e0d54fd8e2041555af09d515738aeca6f
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-05-24 10:56:51 +1000

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,24 @@
+import math
+import random
+import sys
+
+
+def randomTable(n):
+    return [[math.floor(random.random() * 100) for i in range(n)] for j in range(n)]
+
+
+def matrix(event, context):
+    n=int(event['data'])
+    matrixA = randomTable(n)
+    matrixB = randomTable(n)
+    matrixMult = [[0 for i in range(n)] for j in range(n)]
+
+    for i in range(0, len(matrixA)):
+        for j in range(0, len(matrixB)):
+            sum = 0
+            for k in range(0, len(matrixA)):
+                sum += matrixA[i][k] * matrixB[k][j]
+            matrixMult[i][j] = sum
+
+    return matrixMult
+
