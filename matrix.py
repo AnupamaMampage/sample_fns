e476028cf430edf765aa3a7e350c9c0766c6b0ea
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-05-24 11:10:11 +1000

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import math
 import random
 import sys
-
+from time import time
 
 def randomTable(n):
     return [[math.floor(random.random() * 100) for i in range(n)] for j in range(n)]
@@ -11,6 +11,7 @@ def matrix(event, context):
     n=int(event['data'])
     matrixA = randomTable(n)
     matrixB = randomTable(n)
+    start= time()
     matrixMult = [[0 for i in range(n)] for j in range(n)]
 
     for i in range(0, len(matrixA)):
@@ -19,6 +20,9 @@ def matrix(event, context):
             for k in range(0, len(matrixA)):
                 sum += matrixA[i][k] * matrixB[k][j]
             matrixMult[i][j] = sum
-
-    return matrixMult
+            
+    latency= time()- start
+    print matrixMult
+    
+    return latency
 
