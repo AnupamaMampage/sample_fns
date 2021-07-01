6ba82155022815c39ba64ac819cc922ec2391568
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-01 13:25:33 +1000

    Create file.py

---
+++ file.py
@@ -0,0 +1,10 @@
+import numpy as np
+from random import gauss
+
+def file(event, context):
+    a1 = [gauss(1.5, 2) for i in range(10000000000)]
+    import pickle
+    pkl_file = open("serialized_data3.pkl", 'wb')
+
+    pickle.dump(a1, pkl_file)
+    return "FILE OPERATION ENDs"
