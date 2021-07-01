582bebedf76bcfc87365ad537da0968352f760ae
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-01 19:06:58 +1000

    Update speed.py

--- speed.py
+++ speed.py
@@ -1,8 +1,33 @@
 import speedtest
 
-def main(event, context):
-  s = speedtest.Speedtest()
-#   print(dir(speedtest))
-  return (str(s.download()))
+# def main(event, context):
+#   s = speedtest.Speedtest()
+# #   print(dir(speedtest))
+#   return (str(s.download()))
+
+def test():
+    s = speedtest.Speedtest()
+    s.get_servers()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.results.dict()
+    return res["download"], res["upload"], res["ping"]
+
+
+def speed(event,context):
+    # write to csv
+
+    for i in range(3):
+        print('Making test #{}'.format(i+1))
+        d, u, p = test()
+        print("Download: ",d)
+        print("Upload: ", d)
+        print("Ping: ", d)
+        
+     return str(d)
+
+
+
   
 
