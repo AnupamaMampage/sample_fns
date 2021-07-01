d7aece06bbe5d73456d54756239c020a996b1468
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-01 13:27:53 +1000

    Update file.py

--- file.py
+++ file.py
@@ -2,7 +2,7 @@ import numpy as np
 from random import gauss
 
 def file(event, context):
-    a1 = [gauss(1.5, 2) for i in range(10000000000)]
+    a1 = [gauss(1.5, 2) for i in range(10000000)]
     import pickle
     pkl_file = open("serialized_data3.pkl", 'wb')
 
