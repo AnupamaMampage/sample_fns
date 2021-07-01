88833b61455152076ce174c756b7d1d775ec61fb
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-01 14:24:30 +1000

    Update file.py

--- file.py
+++ file.py
@@ -1,12 +1,12 @@
 def file(event, context):
     f = open("demo1.txt", "w")
-    for i in range(100):
+    for i in range(100000):
         line = "This is line %d \n" % i
         f.write(line)
 
     f.close()
     f = open("demo1.txt", "r")
-    for i in range(100):
+    for i in range(100000):
         line = f.read()
 
     f.close()
