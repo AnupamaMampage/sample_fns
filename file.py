035bc3b05358658df8191af2396ac2b29a134602
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-01 17:34:01 +1000

    Update file.py

--- file.py
+++ file.py
@@ -1,17 +1,25 @@
 from random import gauss
 import pickle
+import os
+import random
+import string
 
 def file(event, context):
-    fi = open("kubeless/demo1.txt", "wb")
+    letters = string.ascii_lowercase
+    file_name = ''.join(random.choice(letters) for i in range(10))
+    path = "kubeless/"+ file_name
+    fi = open(path, "wb")
     for i in range(1000):
         a1 = [gauss(1.5, 2) for i in range(10000)]
         pickle.dump(a1, fi)
 
     fi.close()
-    fo = open("kubeless/demo1.txt", "rb")
+    fo = open(path, "rb")
     for i in range(1000):
         a2 = pickle.load(fo)
 
     fo.close()
+    if os.path.exists(path):
+        os.remove(path)    
    
     return "FILE OPERATION ENDs"
