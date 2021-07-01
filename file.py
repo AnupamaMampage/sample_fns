5d94e7cf49e453c6fa87783a7813305c9f383cb3
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-01 15:45:14 +1000

    Update file.py

--- file.py
+++ file.py
@@ -1,13 +1,15 @@
+from random import gauss
+
 def file(event, context):
     f = open("kubeless/demo1.txt", "w")
-    for i in range(1000000):
-        line = "This is line %d \n" % i
-        f.write(line)
+    for i in range(10000):
+        a1 = [gauss(1.5, 2) for i in range(1000000)]
+        f.write(str(a1)+"\n")
 
     f.close()
     f = open("kubeless/demo1.txt", "r")
-    for i in range(1000000):
-        line = f.read()
+    for i in range(10000):
+        a2 = f.read()
 
     f.close()
    
