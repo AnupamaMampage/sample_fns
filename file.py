95256b3cada3796f3bfd3c5df84d1394b0948d46
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-07 13:14:31 +1000

    Update file.py

--- file.py
+++ file.py
@@ -9,8 +9,8 @@ def file(event, context):
     file_name = ''.join(random.choice(letters) for i in range(10))
     path = "kubeless/"+ file_name
     fi = open(path, "wb")
-    for i in range(10000):
-        a1 = [gauss(1.5, 2) for i in range(1000)]
+    for i in range(1000):
+        a1 = [gauss(1.5, 2) for i in range(7500)]
         pickle.dump(a1, fi)
 
 #     fi.close()
