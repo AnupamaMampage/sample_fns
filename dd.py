c7cfcd9d8234edd057a30b46bcadbbad9cb4c198
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-08 11:28:12 +1000

    Update dd.py

--- dd.py
+++ dd.py
@@ -12,8 +12,8 @@ def dd(event, context):
     ip_path = "kubeless/" + ip_file_name
     op_path  = "kubeless/" + op_file_name
     if os.path.exists(ip_path):
-        for i in range(300000):
-            os.system('sudo time dd if=ip_path of=op_path bs=8k count=300000')
+#         for i in range(300000):
+        os.system('sudo time dd if=ip_path of=op_path)
     else:
         fi = open(ip_path, "wb")
         for i in range(1000):
