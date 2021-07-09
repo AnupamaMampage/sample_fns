4b0fc88bbab46f7640c69121f0ab36f7480a952a
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-09 14:39:30 +1000

    Create iperf3.py

---
+++ iperf3.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import time
+from time import time
+
+
+
+def iperf3(event, context):
+    # write to csv
+    start = time()
+    os.system('sudo iperf3 -c 45.113.232.219')
+
+    latency = time() - start
+    return "iperf3: " + str(latency)
