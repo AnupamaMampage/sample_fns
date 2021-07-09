9a7282c823b155fb7e676577928ed948896c5742
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-09 15:36:35 +1000

    Update iperf3.py

--- iperf3.py
+++ iperf3.py
@@ -9,6 +9,7 @@ from time import time
 def iperf3(event, context):
     # write to csv
     start = time()
+    print ("Here")
     os.system('sudo iperf3 -c 45.113.232.219')
 
     latency = time() - start
