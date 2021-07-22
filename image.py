04569f473be1523ee08ea34f2e9368dcd7cb2c3e
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2021-07-22 16:41:29 +1000

    Create image.py

---
+++ image.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import requests
+from io import BytesIO
+from time import time
+
+def load(event, context):
+  basewidth = 300
+  url = 'https://raw.githubusercontent.com/AnupamaMampage/sample_fns/main/cat.jpg'
+  start = time()
+  response = requests.get(url)
+  img = Image.open(BytesIO(response.content))
+  wpercent = (basewidth / float(img.size[0]))
+  hsize = int((float(img.size[1]) * float(wpercent)))
+  img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+
+  latency = time() - start
+  
+  return {"Latency: image: " latency}
