66f75a800c4831e9e81dc985bffd0554e5981379
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2022-06-16 11:40:29 +1000

    Create upload_image.py

---
+++ upload_image.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+def image(event, context):  
+  image = str(event['data'])
+  imgURL = "https://github.com/AnupamaMampage/images/main/" + image + ".jpg"
+  urllib.request.urlretrieve(imgURL,"http://45.113.232.219/data" + image+ ".jpg")
+  
+  return "Image " + image + ".jpg saved"
+  
