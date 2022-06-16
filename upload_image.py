023e0f7bd2d061f874159e43cd1665b748cc4e6e
Anupama Mampage <64462054+AnupamaMampage@users.noreply.github.com>
2022-06-16 12:01:51 +1000

    Update upload_image.py

--- upload_image.py
+++ upload_image.py
@@ -2,7 +2,7 @@ import urllib.request
 
 def image(event, context):  
   image = str(event['data'])
-  imgURL = "https://github.com/AnupamaMampage/images/main/" + image + ".jpg"
+  imgURL = "https://raw.githubusercontent.com/AnupamaMampage/images/main/" + image + ".jpg"
   urllib.request.urlretrieve(imgURL,"http://45.113.232.219/data" + image+ ".jpg")
   
   return "Image " + image + ".jpg saved"
